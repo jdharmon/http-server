0e55b7d260c05fc9af778447a774c6e729c80beb
Jason Harmon <jason@jdharmon.com>
2020-04-21 10:35:20 -0400

    Add response body option

--- main.go
+++ main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
-func handle(response http.ResponseWriter, request *http.Request) {
-	body, err := ioutil.ReadAll(request.Body)
+var responseBody *string
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		response.WriteHeader(500)
-		io.WriteString(response, "Unable to read body.")
+		w.WriteHeader(500)
+		io.WriteString(w, "Unable to read body.")
 		return
 	}
 
-	fmt.Printf("%s %s\n", request.Method, request.URL)
-	for header, value := range request.Header {
+	fmt.Printf("%s %s\n", r.Method, r.URL)
+	for header, value := range r.Header {
 		fmt.Printf("%s: %s\n", header, value[0])
 	}
 	fmt.Printf("\n%s\n\n\n", body)
 
-	response.WriteHeader(200)
+	io.WriteString(w, *responseBody)
 }
 
 func main() {
-	portNumber := os.Getenv("LISTEN_PORT")
-	if portNumber == "" {
-		portNumber = "80"
-	}
+	portNumber := flag.String("port", "80", "Port to listen on")
+	responseBody = flag.String("response", "", "String response")
+	flag.Parse()
+
 	http.HandleFunc("/", handle)
-	fmt.Println("Server listening on port", portNumber)
-	http.ListenAndServe(":"+portNumber, nil)
+	fmt.Println("Server listening on port", *portNumber)
+	if *responseBody != "" {
+		fmt.Println("Response:", *responseBody)
+	}
+
+	http.ListenAndServe(":"+*portNumber, nil)
 }
